6ee6fb7cdbc3971f10f5a76a243dbe152bf8b29e
Matt Galvin <matt.t.galvin@gmail.com>
2017-02-14 22:05:29 -0500

    change app id

--- platforms/android/src/com/nbcu/corpsys/nbcunow/directory/MainActivity.java
+++ platforms/android/src/com/nbcu/corpsys/nbcunow/directory/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package com.inbcu.corpsys.nbcunow.directory;
+package com.nbcu.corpsys.nbcunow.directory;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
